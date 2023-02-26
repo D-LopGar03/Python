6670fab060732ff756b4b08074c3331cf0426105
D-LopGar03 <damianlopez613@gmail.com>
2023-02-26 09:38:29 -0500

    Se avanza en el proyecto 25-02-2023 para la entrega del mismo el 27-02-2023

--- 25-02-2023/Family.py
+++ 25-02-2023/Family.py
@@ -11,10 +11,7 @@ for i in range(3):
     parents.append(mother)
     answer = input("¿Tienen hijos? s/n: ")
     
-    if answer == "n":
-        valor = 0
-        cantSons.append(valor)        
-    elif answer == "s":
+    if answer == "s" or answer == "S":
         sons.append(valor)
         valor=int(input("¿Cuántos hijos tienen?: "))
         cantSons.append(valor)
@@ -22,13 +19,30 @@ for i in range(3):
             print("Ingrese el nombre del hijo ", (h+1))
             hijo = str(input())
             sons.append(hijo)
-  
-            
-cantidadHijos = len(cantSons)            
+    elif answer == "n" or answer == "N":
+        valor = 0
+        cantSons.append(valor)      
+               
+print("Listado del padre y la cantidad de hijos qué tiene: ")            
+""" cantidadHijos = len(cantSons)    """         
 for j in range(0, 6, 2):
-    print("Padre: ", parents[j], cantSons[j])
+    print("Padre: ", parents[j])
+    if j == 0:
+        for x in range(1):
+            print("Cantidad de hijos: ",cantSons[0])
+    elif j == 2:
+        for x in range(1):
+            print("Cantidad de hijos: ",cantSons[1])
+    elif j == 4:
+        for x in range(1):
+            print("Cantidad de hijos: ",cantSons[2])
     
-    
-
-
-    
+print("Listado del padre, madre e hijos: ")  
+cantidadHijos = len(cantSons) 
+for padres in range(1,7,2):
+    if padres == 0:
+        print("Padre: ", parents(0))
+        print("Madre: ", parents(1))
+        
+        for hijos in range(cantidadHijos):
+            print("Hijo: ", sons(hijos))
