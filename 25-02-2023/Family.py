d9423d6fad4fb42c4aa13b3bb5d0d2456e9e2328
Damian López García <103325885+D-LopGar03@users.noreply.github.com>
2023-02-27 13:50:32 -0500

    Add files via upload

--- 25-02-2023/Family.py
+++ 25-02-2023/Family.py
@@ -1,48 +1,86 @@
-parents = []
-sons = []
-cantSons = []
-valor = 0
-x = 0
+parents1 = []
+parents2 = []
+parents3 = []
+sons1 = []
+sons2 = []
+sons3 = []
+a = 0
 
-for i in range(3):
-    father = input("Nombre del padre: ")
-    mother = input("Nombre de la madre: ")
-    parents.append(father)
-    parents.append(mother)
-    answer = input("¿Tienen hijos? s/n: ")
-    
+
+for f in range (1):
+    father = str(input("Ingrese el nombre del padre: "))
+    mother = str(input("Ingrese el nombre de la madre: "))
+    parents1.append(father)
+    parents1.append(mother)
+    answer = str(input("¿Tienen hijos? s/n: "))
     if answer == "s" or answer == "S":
-        sons.append(valor)
-        valor=int(input("¿Cuántos hijos tienen?: "))
-        cantSons.append(valor)
-        for h in range(valor):
-            print("Ingrese el nombre del hijo ", (h+1))
-            hijo = str(input())
-            sons.append(hijo)
+        cant = int(input("¿Cuántos hijos tienen?: "))
+        for cantidad in range(cant):
+            print("Ingrese el nombre del hijo ", (cantidad + 1))
+            hijo = str(input(""))
+            sons1.append(hijo)
     elif answer == "n" or answer == "N":
         valor = 0
-        cantSons.append(valor)      
-               
-print("Listado del padre y la cantidad de hijos qué tiene: ")            
-""" cantidadHijos = len(cantSons)    """         
-for j in range(0, 6, 2):
-    print("Padre: ", parents[j])
-    if j == 0:
-        for x in range(1):
-            print("Cantidad de hijos: ",cantSons[0])
-    elif j == 2:
-        for x in range(1):
-            print("Cantidad de hijos: ",cantSons[1])
-    elif j == 4:
-        for x in range(1):
-            print("Cantidad de hijos: ",cantSons[2])
-    
-print("Listado del padre, madre e hijos: ")  
-cantidadHijos = len(cantSons) 
-for padres in range(1,7):
+        sons1.append(valor)  
 
-    print("Padre: ", parents[0])
-    print("Madre: ", parents[1])
-    
-    for hijos in range(cantidadHijos):
-        print("Hijo: ", sons[hijos])
+for f in range (1):
+    father = str(input("Ingrese el nombre del padre: "))
+    mother = str(input("Ingrese el nombre de la madre: "))
+    parents2.append(father)
+    parents2.append(mother)
+    answer = str(input("¿Tienen hijos? s/n: "))
+    if answer == "s" or answer == "S":
+        cant = int(input("¿Cuántos hijos tienen?: "))
+        for cantidad in range(cant):
+            print("Ingrese el nombre del hijo ", (cantidad + 1))
+            hijo = str(input(""))
+            sons2.append(hijo)
+    elif answer == "n" or answer == "N":
+        valor = 0
+        sons2.append(valor) 
+
+for f in range (1):
+    father = str(input("Ingrese el nombre del padre: "))
+    mother = str(input("Ingrese el nombre de la madre: "))
+    parents3.append(father)
+    parents3.append(mother)
+    answer = str(input("¿Tienen hijos? s/n: "))
+    if answer == "s" or answer == "S":
+        cant = int(input("¿Cuántos hijos tienen?: "))
+        for cantidad in range(cant):
+            print("Ingrese el nombre del hijo ", (cantidad + 1))
+            hijo = str(input(""))
+            sons3.append(hijo)
+    elif answer == "n" or answer == "N":
+        valor = 0
+        sons3.append(valor) 
+
+longitud1 = len(sons1)
+print("Padre: ", parents1[0])
+print("Cantidad de hijos: ", longitud1)
+
+longitud2 = len(sons2)
+print("Padre: ", parents2[0])
+print("Cantidad de hijos: ", longitud2)
+
+longitud3 = len(sons2)
+print("Padre: ", parents3[0])
+print("Cantidad de hijos: ", longitud3)
+
+for f in range(1):
+    print("Padre: ", parents1[f])
+    print("Madre: ", parents1[f + 1])
+    for g in range(longitud1):
+        print("Hijo: ", sons1[g])
+
+for f in range(1):
+    print("Padre: ", parents2[f])
+    print("Madre: ", parents2[f + 1])
+    for g in range(longitud2):
+        print("Hijo: ", sons2[g])
+
+for f in range(1):
+    print("Padre: ", parents3[f])
+    print("Madre: ", parents3[f + 1])
+    for g in range(longitud3):
+        print("Hijo: ", sons2[g])
