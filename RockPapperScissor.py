69460cbac80ad0b116c07c4ec374de1e63a07fc9
D-LopGar03 <damianlopez613@gmail.com>
2023-03-21 20:12:30 -0500

    Se corrige error del código en RockPapperScissor.py para que el juego se reinicie si el jugador lo desea

--- RockPapperScissor.py
+++ RockPapperScissor.py
@@ -1,50 +1,69 @@
 import random
 import os
 
-from os import system
-system("cls")
+partida = 1
 
-selectUser = input(str("Piedra, papel o tijeras: "))
-selectUserModify = selectUser.lower()
+while partida == 1:
+    def clear():
+        if os.name == "nt":
+            os.system("cls")
+        else:
+            os.system("clear")
+    clear() 
 
-opciones = ['Papel', 'Piedra', 'Tijeras']
+    selectUser = input(str("Piedra, papel o tijeras: "))
+    selectUserModify = selectUser.lower()
 
-seleccion = (random.choice(opciones))
-seleccionModify = seleccion.lower()
+    opciones = ['Papel', 'Piedra', 'Tijeras']
 
-#POSIBLES SELECCIONES DEL USUARIO CON PIEDRA
+    seleccion = (random.choice(opciones))
+    seleccionModify = seleccion.lower()
 
-if selectUserModify == "piedra" and seleccion == "Piedra":
-    print(selectUserModify, " vs ", seleccionModify)
-    print("Empate")        
-elif selectUserModify == "piedra" and seleccion == "Papel":
-    print(selectUserModify, " vs ", seleccionModify)
-    print("Yo gano")
-elif selectUserModify == "piedra" and seleccion == "Tijeras":
-    print(selectUserModify, " vs ", seleccionModify)
-    print("Yo pierdo")
+    #POSIBLES SELECCIONES DEL USUARIO CON PIEDRA
 
-#POSIBLES SELECCIONES DEL USUARIO CON PAPEL
+    if selectUserModify == "piedra" and seleccion == "Piedra":
+        print(selectUserModify, " vs ", seleccionModify)
+        print("Empate")        
+    elif selectUserModify == "piedra" and seleccion == "Papel":
+        print(selectUserModify, " vs ", seleccionModify)
+        print("Yo gano")
+    elif selectUserModify == "piedra" and seleccion == "Tijeras":
+        print(selectUserModify, " vs ", seleccionModify)
+        print("Yo pierdo")
 
-elif selectUserModify == "papel" and seleccion == "Papel":
-    print(selectUserModify, " vs ", seleccionModify)
-    print("Empate") 
-elif selectUserModify == "papel" and seleccion == "Piedra":
-    print(selectUserModify, " vs ", seleccionModify)
-    print("Yo pierdo")
-elif selectUserModify == "papel" and seleccion == "Tijeras":
-    print(selectUserModify, " vs ", seleccionModify)
-    print("Yo gano")
+    #POSIBLES SELECCIONES DEL USUARIO CON PAPEL
 
-#POSIBLES SELECCIONES DEL USUARIO CON TIJERAS
+    elif selectUserModify == "papel" and seleccion == "Papel":
+        print(selectUserModify, " vs ", seleccionModify)
+        print("Empate") 
+    elif selectUserModify == "papel" and seleccion == "Piedra":
+        print(selectUserModify, " vs ", seleccionModify)
+        print("Yo pierdo")
+    elif selectUserModify == "papel" and seleccion == "Tijeras":
+        print(selectUserModify, " vs ", seleccionModify)
+        print("Yo gano")
 
-elif selectUserModify == "tijeras" and seleccion == "Papel":
-    print(selectUserModify, " vs ", seleccionModify)
-    print("Yo pierdo") 
-elif selectUserModify == "tijeras" and seleccion == "Piedra":
-    print(selectUserModify, " vs ", seleccionModify)
-    print("Yo gano")
-elif selectUserModify == "tijeras" and seleccion == "Tijeras":
-    print(selectUserModify, " vs ", seleccionModify)
-    print("Empate")
+    #POSIBLES SELECCIONES DEL USUARIO CON TIJERAS
 
+    elif selectUserModify == "tijeras" and seleccion == "Papel":
+        print(selectUserModify, " vs ", seleccionModify)
+        print("Yo pierdo") 
+    elif selectUserModify == "tijeras" and seleccion == "Piedra":
+        print(selectUserModify, " vs ", seleccionModify)
+        print("Yo gano")
+    elif selectUserModify == "tijeras" and seleccion == "Tijeras":
+        print(selectUserModify, " vs ", seleccionModify)
+        print("Empate")
+
+    print(f"""
+        1. Para reiniciar
+        2. Para salir    
+    """)
+    partida = int(input())
+
+def clear():
+        if os.name == "nt":
+            os.system("cls")
+        else:
+            os.system("clear")
+clear()
