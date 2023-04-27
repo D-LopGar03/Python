b4d25c02623d1b0fd7602472170f0f740afe9193
Damian López García <103325885+D-LopGar03@users.noreply.github.com>
2023-04-27 13:42:20 -0500

    Delete Python directory

--- Python/Clase8-Entrada_De_Datos2.py
+++
@@ -1,9 +0,0 @@
-nombre = (input("¿Cómo te llamas?: "))
-print("Hola", nombre, " este programa realizará una suma. ")
-numero1 = int(input("Ingresa el primer número entero: "))
-numero2 = int(input("Ingresa el segundo número entero: "))
-
-resultado = numero1 + numero2
-
-print(nombre, "el resultado es: ", resultado)
-
