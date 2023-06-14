163681bc1df2b183fba34589d8785ebf03f74f34
Damian López García <103325885+D-LopGar03@users.noreply.github.com>
2023-06-13 20:51:13 -0500

    Add files via upload

--- Cierre de mesa/cierre.py
+++ Cierre de mesa/cierre.py
@@ -5,6 +5,6 @@ radicado = int(input("Número del radicado de la solicitud: "))
 
 motivo = int(input(f"\n1). Activación de Office.\n2). Computador lento\n> "))
 
-user = str(input("Nombre del usuario: "))
+user = (input("Nombre del usuario: "))
 
 user = user.capitalize()
--- Cierre de mesa/funciones.py
+++ Cierre de mesa/funciones.py
@@ -1,13 +1,15 @@
 from cierre import user, motivo, radicado
 from motivos import motivos
-import webbrowser
-import datetime
+import webbrowser, datetime, sys, pyautogui, pyperclip, time
+
+
+path = 'documento'
 
 hora_actual = datetime.datetime.now().time()
 
 # Definir los límites para "buenos días" y "buenas tardes"
 hora_inicio_tarde = datetime.time(12, 0, 0)  # Por ejemplo, a partir de las 12:00:00 se considera "tarde"
-hora_inicio_noche = datetime.time(19, 0, 0)  # Por ejemplo, a partir de las 18:00:00 se considera "noche"
+hora_inicio_noche = datetime.time(23, 0, 0)  # Por ejemplo, a partir de las 18:00:00 se considera "noche"
 
 # Comparar la hora actual con los límites
 if hora_actual < hora_inicio_tarde:
@@ -20,28 +22,40 @@ else:
     hora = f"Buenas noches, {user}\n\n"
     despedida = ("\n\nFeliz noche.")
 
-def concatenar(hora, motivo, despedida):
+def concatenar(hora, motivo, despedida, path):
 
     if motivo == 1 and hora_actual < hora_inicio_tarde:
 
-        with open("documento.txt", "w") as f:
-            f.write ({hora}, {motivos[1]}, {despedida})
+        hora = str(hora)
+        despedida = str(despedida)
+        sys.stdout = open(path, 'w')
+        print(f"{hora}{motivos[1]}{despedida}")
        
 
     elif motivo == 1 and hora_actual < hora_inicio_noche:
-         entradas = str({hora}, {motivos[1]}, {despedida})
-         with open("documento.txt", "w") as f:
-            f.write (entradas)
+                  
+         hora = str(hora)
+         despedida = str(despedida)
+         sys.stdout = open(path, 'w')
+         print(f"{hora}{motivos[1]} {despedida}")
          
-         print(f)
     
 
-concatenar(hora, motivo, despedida)
+concatenar(hora, motivo, despedida, path)
 
 
 
 def buscador(radicado):
     webbrowser.open_new(f"https://mesadeservicio.autonoma.edu.co/avance/crear/{radicado}")
+    time.sleep(3)
+
 buscador(radicado)
+
+def abrirDoc(path='documento'):
+    with open('documento', 'r'):
+        for i in path:
+            pyperclip.copy(i)
+
+abrirDoc(path)
 
 exit(0)
